13d5ef2c23c621ef9ac1a1f47e39696ebcfda329
Vavarooks <vavarooks123@gmail.com>
2020-01-25 14:44:16 -0800

    Java moved, Css added

--- assets/Java.js
+++ assets/Java.js
@@ -2,29 +2,30 @@ var timeEl = document.querySelector(".time");
 
 var mainEl = document.getElementById("main");
 
-var oneBtn = document.getElementById("one");
+var startBtn = document.getElementById("start");
 
-var twoBtn = document.getElementById("two");
+if (startBtn.addEventListener("click")) {
 
-var threeBtn = document.getElementById("three");
+  var secondsLeft = 76;
 
-var fourBtn = document.getElementById("four");
+  function setTime() {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left.";
+  
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        alert("Times Up!");
+      }
+  
+    }, 1000);
+  }
+  setTime();
 
-var secondsLeft = 76;
+};
 
-function setTime() {
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left.";
 
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
 
-  }, 1000);
-}
-setTime();
 
 var question1 =
 {
@@ -58,4 +59,10 @@ var question5 = {
   answer: "Regimon",
 };
 
+// var oneBtn = document.getElementById("one");
 
+// var twoBtn = document.getElementById("two");
+
+// var threeBtn = document.getElementById("three");
+
+// var fourBtn = document.getElementById("four");
