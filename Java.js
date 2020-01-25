92b17c22285d47ed3ad7013e1bd16fee58d76c6b
Vavarooks <vavarooks123@gmail.com>
2020-01-25 09:52:06 -0800

    HTML edit, Java edit

--- Java.js
+++ Java.js
@@ -1,3 +1,35 @@
+var timeEl = document.querySelector(".time");
+
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 160;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// function sendMessage() {
+//   timeEl.textContent = " ";
+
+//   var imgEl = document.createElement("img");
+
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+setTime();
+
+
 var questions = 
     {
       title: "Commonly used data types DO NOT include:",
